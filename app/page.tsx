18fdcc2fc94aaa34f15ce279d8edd2800fc7c6ce
manashjbofficial <mjbaruah10@gmail.com>
2023-09-16 20:46:37 +0530

    conditional profile rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useState, useId } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import background from "../public/gradient.png";
 
@@ -66,8 +66,8 @@ const Page = () => {
       </div>
       <div className="absolute inset-0  bg-white opacity-60 z-0"></div>
       <NavBar signIn={signIn} signOut={signOut} session={session} />
-      <div className="flex flex-col h-screen items-center justify-center  z-10 md:px-10">
-        <div className="flex items-center justify-center mx-4 p-8 mb-4">
+      <div className="flex flex-col h-screen items-center justify-start  z-10 md:px-10">
+        <div className="flex items-center justify-start mx-4 p-8 mb-4 pt-unit-40">
           <h1 className="text-2xl sm:text-4xl md:text-5xl xl:text-6xl font-bold mb-8 bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text text-transparent">
             Url Shortener
           </h1>
@@ -77,17 +77,6 @@ const Page = () => {
           onSubmit={handleSubmit}
           className="flex flex-row w-full max-w-sm items-center justify-center space-x-2 px-5 py-10 sm:px-0 md:px-0 xl:px-0"
         >
-          {/* <Input
-            type="url"
-            placeholder="Shorten your link"
-            className="shadow-lg border-black bg-slate-50"
-            name="long_url"
-            value={longUrl}
-            onChange={(e) => setLongUrl(e.target.value)}
-            pattern="https://.*"
-            title="Please enter a valid URL starting with 'https://'"
-            required
-          /> */}
           <Input
             isRequired
             isClearable
